b11472f73672f8b70c2c06a21708069f754c523f
Jire <thomasgnappo@gmail.com>
2024-08-01 03:49:17 -0400

    Updated `game-client` `fastutil` version to latest (`8.5.14`)

--- game-client/build.gradle.kts
+++ game-client/build.gradle.kts
@@ -80,7 +80,7 @@ dependencies {
 
     implementation(files("libs/SwiftFUP-client-1.9.1.jar"))
     implementation(files("libs/allatori-annotations.jar"))
-    implementation("it.unimi.dsi:fastutil:8.5.12")
+    implementation("it.unimi.dsi:fastutil:8.5.14")
     implementation("com.displee:rs-cache-library:6.9")
 
     val netty = "4.1.112.Final"
