c9bbf93a902816e6fa451eaf99f31a945203cd62
Jire <thomasgnappo@gmail.com>
2024-08-01 03:27:29 -0400

    Updated `game-server/run.bat` to correct Shadow all JAR file name

--- game-server/run.bat
+++ game-server/run.bat
@@ -1,4 +1,4 @@
 @echo off
 title Tarnish Server
-java -XX:-OmitStackTraceInFastThrow --enable-preview -XX:+UseZGC -Xmx8g -Xms4g -jar build\libs\os-royale-server-all.jar
+java -XX:-OmitStackTraceInFastThrow --enable-preview -XX:+UseZGC -Xmx8g -Xms4g -jar build\libs\game-server-all.jar
 pause
